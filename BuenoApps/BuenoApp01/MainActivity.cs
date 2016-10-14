e21d3eff91d179bd737067674f580d998a412b97
luizhhbueno <luizhenrique8181@gmail.com>
2016-10-14 13:39:23 +0200

    text and switch01

--- BuenoApps/BuenoApp01/MainActivity.cs
+++ BuenoApps/BuenoApp01/MainActivity.cs
@@ -8,7 +8,7 @@ namespace BuenoApp01
 	public class MainActivity : Activity
 	{
 		int count = 1;
-
+		Switch switch1;
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
@@ -21,6 +21,24 @@ namespace BuenoApp01
 			Button button = FindViewById<Button>(Resource.Id.myButton);
 
 			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+			switch1 = FindViewById<Switch>(Resource.Id.switch1);
+			switch1.CheckedChange += Switch1_CheckedChange;
+			var text1 = FindViewById<TextView>(Resource.Id.textView1);
+			text1.Visibility = Android.Views.ViewStates.Invisible;
+		}
+
+		void Switch1_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
+		{
+			var text1 = FindViewById<TextView>(Resource.Id.textView1);
+			if (switch1.Checked)
+			{
+				text1.Visibility = Android.Views.ViewStates.Visible;
+			}
+			else
+			{
+				text1.Visibility = Android.Views.ViewStates.Invisible;
+			}
 		}
 	}
 }
